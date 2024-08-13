d88a9fa245f1c5877ca590041ba755ec66a548e0
alekseipeikhel <alekseipeikhel@gmail.com>
2024-08-13 11:01:16 +0300

    исправление импорта модуля

---
+++ src/meal-plan-service/src/main/java/com/plan_menu/meal_plan_service/MealPlanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.plan_menu.meal_plan_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlanServiceApplication.class, args);
+	}
+
+}

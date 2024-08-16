27c7479c8f4fd1f4083df19042ddf379870d1e23
alekseipeikhel <alekseipeikhel@gmail.com>
2024-08-16 19:05:52 +0300

    простой exception

---
+++ src/meal-plan-service/src/main/java/com/plan_menu/meal_plan_service/exception/EntityNotFoundException.java
@@ -0,0 +1,15 @@
+package com.plan_menu.meal_plan_service.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+    /**
+     * Constructs a new runtime exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}

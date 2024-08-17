06563db3f19c17c61a3b2799f53802ac76c4821b
alekseipeikhel <alekseipeikhel@gmail.com>
2024-08-17 07:25:17 +0300

    внедрение мапперов в контекст разорвана зависимость между ними

--- src/meal-plan-service/src/main/java/com/plan_menu/meal_plan_service/mapper/impl/MapperMealPlanEntryImpl.java
+++ src/meal-plan-service/src/main/java/com/plan_menu/meal_plan_service/mapper/impl/MapperMealPlanEntryImpl.java
@@ -3,15 +3,18 @@ package com.plan_menu.meal_plan_service.mapper.impl;
 import com.plan_menu.meal_plan_service.dto.MealPlanEntryDto;
 import com.plan_menu.meal_plan_service.entity.MealPlanEntryEntity;
 import com.plan_menu.meal_plan_service.mapper.MapperMealPlanEntry;
+import org.springframework.stereotype.Component;
 
 /**
  * Реализация интерфейса MapperMealPlanEntry для преобразования
  * между сущностью MealPlanEntryEntity и DTO MealPlanEntryDto.
  */
+@Component
 public class MapperMealPlanEntryImpl implements MapperMealPlanEntry {
 
     @Override
     public MealPlanEntryDto mapToDto(MealPlanEntryEntity entity) {
+        if (entity == null) return null;
         // Преобразование данных из сущности в DTO
         return new MealPlanEntryDto(
                 entity.getRecipeId(),
@@ -23,6 +26,7 @@ public class MapperMealPlanEntryImpl implements MapperMealPlanEntry {
 
     @Override
     public MealPlanEntryEntity mapToEntity(MealPlanEntryDto dto) {
+        if (dto == null) return null;
         // Создание новой сущности и заполнение её данными из DTO
         MealPlanEntryEntity entryEntity = new MealPlanEntryEntity();
 
--- src/meal-plan-service/src/main/java/com/plan_menu/meal_plan_service/mapper/impl/MapperMealPlanImp.java
+++ src/meal-plan-service/src/main/java/com/plan_menu/meal_plan_service/mapper/impl/MapperMealPlanImp.java
@@ -3,13 +3,14 @@ package com.plan_menu.meal_plan_service.mapper.impl;
 import com.plan_menu.meal_plan_service.dto.MealPlanDto;
 import com.plan_menu.meal_plan_service.dto.MealPlanEntryDto;
 import com.plan_menu.meal_plan_service.entity.MealPlanEntity;
-import com.plan_menu.meal_plan_service.entity.MealPlanEntryEntity;
 import com.plan_menu.meal_plan_service.mapper.MapperMealPlan;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class MapperMealPlanImp implements MapperMealPlan {
-    
+
     @Override
     public MealPlanDto mapToDto(MealPlanEntity entity) {
         // Преобразуем список MealPlanEntryEntity в список MealPlanEntryDto
@@ -33,19 +34,11 @@ public class MapperMealPlanImp implements MapperMealPlan {
 
     @Override
     public MealPlanEntity mapToEntity(MealPlanDto dto) {
-        // Создаем экземпляр MapperMealPlanEntryImpl для преобразования записей плана
-        MapperMealPlanEntryImpl mapperMealPlanEntry = new MapperMealPlanEntryImpl();
         // Создаем новый объект сущности MealPlanEntity
         MealPlanEntity mealPlanEntity = new MealPlanEntity();
-        // Преобразуем список MealPlanEntryDto в список MealPlanEntryEntity
-        List<MealPlanEntryEntity> entities = dto.mealPlanEntry()
-                .stream()
-                .map(mapperMealPlanEntry::mapToEntity)
-                .toList();
         // Устанавливаем userId в сущности
         mealPlanEntity.setUserId(dto.userId());
-        // Устанавливаем список записей плана в сущности
-        mealPlanEntity.setMealPlanEntryEntityList(entities);
+        mealPlanEntity.setMealPlanEntryEntityList(List.of());
         // Возвращаем объект сущности с преобразованными данными
         return mealPlanEntity;
     }

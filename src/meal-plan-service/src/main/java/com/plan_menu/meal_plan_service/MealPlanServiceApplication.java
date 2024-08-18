80fe191679276cb51eac2b38729b88a40b9d372f
alekseipeikhel <alekseipeikhel@gmail.com>
2024-08-18 11:56:33 +0300

    включение еврики

--- src/meal-plan-service/src/main/java/com/plan_menu/meal_plan_service/MealPlanServiceApplication.java
+++ src/meal-plan-service/src/main/java/com/plan_menu/meal_plan_service/MealPlanServiceApplication.java
@@ -2,12 +2,14 @@ package com.plan_menu.meal_plan_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MealPlanServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MealPlanServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MealPlanServiceApplication.class, args);
+    }
 
 }

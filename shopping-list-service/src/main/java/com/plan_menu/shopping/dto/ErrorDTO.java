4afccfb6e9eaf1fa082a50cb6e7f28fb735a8aaf
Vlad <mitskevichvlm@gmail.com>
2024-08-19 01:47:19 +0300

    feat: little refactoring erdto

--- shopping-list-service/src/main/java/com/plan_menu/shopping/dto/ErrorDTO.java
+++ shopping-list-service/src/main/java/com/plan_menu/shopping/dto/ErrorDTO.java
@@ -2,9 +2,10 @@ package com.plan_menu.shopping.dto;
 
 /**
  * Объект передачи данных (DTO) для передачи информации об ошибках.
- * Этот DTO содержит код ошибки, сообщение об ошибке и дополнительные детали.
+ * Этот DTO содержит идентификатор пользователя, код ошибки, сообщение об ошибке и дополнительные детали.
  */
 public record ErrorDTO(
+        Long userId,
         String errorCode,
         String message,
         String details) {

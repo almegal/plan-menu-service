0fd48a58e81c52b7b5475475f4b5eab20675b74e
Vlad <mitskevichvlm@gmail.com>
2024-08-20 22:07:44 +0300

    feat: add some refactoring

--- shopping-list-service/src/main/java/com/plan_menu/shopping/service/impl/ShoppingListServiceImpl.java
+++ shopping-list-service/src/main/java/com/plan_menu/shopping/service/impl/ShoppingListServiceImpl.java
@@ -9,6 +9,7 @@ import com.plan_menu.shopping.entity.ShoppingListItem;
 import com.plan_menu.shopping.exception.ShoppingListNotFoundException;
 import com.plan_menu.shopping.feign.MenuPlannerClient;
 import com.plan_menu.shopping.feign.NotificationClient;
+import com.plan_menu.shopping.mapper.ShoppingListMapper;
 import com.plan_menu.shopping.repository.ProductRepository;
 import com.plan_menu.shopping.repository.ShoppingListRepository;
 import com.plan_menu.shopping.service.NotificationService;
@@ -35,6 +36,7 @@ public class ShoppingListServiceImpl implements ShoppingListService {
     private final ProductRepository productRepository;
     private final MenuPlannerClient menuPlannerClient;
     private final NotificationClient notificationClient;
+    private final ShoppingListMapper shoppingListMapper;
 
     /**
      * Конструктор для внедрения зависимостей.
@@ -45,78 +47,78 @@ public class ShoppingListServiceImpl implements ShoppingListService {
      * @param productRepository репозиторий для работы с продуктами
      * @param menuPlannerClient клиент для взаимодействия с Meal Planning Service
      * @param notificationClient клиент для отправки уведомлений
+     * @param shoppingListMapper маппер для преобразования между сущностями и DTO
      */
     public ShoppingListServiceImpl(ShoppingListRepository shoppingListRepository,
                                    NotificationService notificationService,
                                    ProductService productService,
                                    ProductRepository productRepository,
                                    MenuPlannerClient menuPlannerClient,
-                                   NotificationClient notificationClient) {
+                                   NotificationClient notificationClient,
+                                   ShoppingListMapper shoppingListMapper) {
         this.shoppingListRepository = shoppingListRepository;
         this.notificationService = notificationService;
         this.productService = productService;
         this.productRepository = productRepository;
         this.menuPlannerClient = menuPlannerClient;
         this.notificationClient = notificationClient;
+        this.shoppingListMapper = shoppingListMapper;
     }
 
     @Override
     public ShoppingListResponseDTO createShoppingList(ShoppingListRequestDTO requestDTO) {
         // Получение данных от Meal Planning Service
-        ShoppingListResponseDTO mealPlan = menuPlannerClient.getMealPlanById(requestDTO.mealPlanId());
+        var mealPlan = menuPlannerClient.getMealPlanById(requestDTO.mealPlanId());
 
         // Создание списка покупок на основе mealPlan
-        ShoppingList shoppingList = new ShoppingList();
-        shoppingList.setName(requestDTO.name());
-        shoppingList.setDescription(requestDTO.description());
+        var shoppingList = shoppingListMapper.toShoppingList(requestDTO);
         shoppingList.setCreatedDate(LocalDateTime.now());
-        shoppingList.setStatus("NEW");
         shoppingList = shoppingListRepository.save(shoppingList);
 
         // Отправка уведомления пользователю
-        NotificationRequestDTO notificationRequestDTO = new NotificationRequestDTO(
+        var notificationRequestDTO = new NotificationRequestDTO(
                 requestDTO.userId(),
                 "Ваш список покупок создан",
                 "INFO"
         );
         notificationClient.sendNotification(notificationRequestDTO);
 
-        return convertToResponseDTO(shoppingList);
+        return shoppingListMapper.toShoppingListResponseDTO(shoppingList);
     }
 
     @Override
     public ShoppingListResponseDTO getShoppingListById(Long id) {
-        ShoppingList shoppingList = shoppingListRepository.findById(id)
+        var shoppingList = shoppingListRepository.findById(id)
                 .orElseThrow(() -> new ShoppingListNotFoundException("Shopping List not found with id: " + id));
-        return convertToResponseDTO(shoppingList);
+        return shoppingListMapper.toShoppingListResponseDTO(shoppingList);
     }
 
     @Override
     public ShoppingListResponseDTO optimizeShoppingList(Long shoppingListId) {
-        ShoppingList shoppingList = shoppingListRepository.findById(shoppingListId)
+        var shoppingList = shoppingListRepository.findById(shoppingListId)
                 .orElseThrow(() -> new ShoppingListNotFoundException("Shopping List not found with id: " + shoppingListId));
 
-        for (ShoppingListItem item : shoppingList.getItems()) {
+        for (var item : shoppingList.getItems()) {
             int requiredQuantity = item.getQuantity();
             Long productId = item.getProduct().getId();
 
             // Получаем список доступных упаковок для данного продукта
-            List<Product> availableProducts = productRepository.findAvailableProductsByProductId(productId);
+            var availableProducts = productRepository.findAvailableProducts(productId);
 
             // Оптимизируем количество с использованием метода оптимального подбора
-            List<Product> optimizedProducts = findOptimalCombination(availableProducts, requiredQuantity);
+            var optimizedProducts = findOptimalCombination(availableProducts, requiredQuantity);
 
             // Обновляем список покупок
             item.setOptimizedProducts(optimizedProducts);
         }
 
         shoppingList = shoppingListRepository.save(shoppingList);
-        return convertToResponseDTO(shoppingList);
+        return shoppingListMapper.toShoppingListResponseDTO(shoppingList);
     }
 
     @Override
     public void initiateProductOrder(Long shoppingListId) {
-        ShoppingList shoppingList = shoppingListRepository.findById(shoppingListId)
+        var shoppingList = shoppingListRepository.findById(shoppingListId)
                 .orElseThrow(() -> new ShoppingListNotFoundException("Shopping List not found with id: " + shoppingListId));
 
         shoppingList.setStatus("ORDER_INITIATED");
@@ -127,7 +129,7 @@ public class ShoppingListServiceImpl implements ShoppingListService {
 
     @Override
     public void startProductCollection(Long shoppingListId) {
-        ShoppingList shoppingList = shoppingListRepository.findById(shoppingListId)
+        var shoppingList = shoppingListRepository.findById(shoppingListId)
                 .orElseThrow(() -> new ShoppingListNotFoundException("Shopping List not found with id: " + shoppingListId));
 
         shoppingList.setStatus("COLLECTION_STARTED");
@@ -138,7 +140,7 @@ public class ShoppingListServiceImpl implements ShoppingListService {
 
     @Override
     public void startProductDelivery(Long shoppingListId) {
-        ShoppingList shoppingList = shoppingListRepository.findById(shoppingListId)
+        var shoppingList = shoppingListRepository.findById(shoppingListId)
                 .orElseThrow(() -> new ShoppingListNotFoundException("Shopping List not found with id: " + shoppingListId));
 
         shoppingList.setStatus("DELIVERY_STARTED");
@@ -149,7 +151,7 @@ public class ShoppingListServiceImpl implements ShoppingListService {
 
     @Scheduled(fixedDelay = 15000) // Задержка в 15 секунд
     public void processOrder() {
-        Optional<ShoppingList> shoppingListOpt = shoppingListRepository.findFirstByStatus("NEW");
+        var shoppingListOpt = shoppingListRepository.findFirstByStatus("NEW");
         shoppingListOpt.ifPresent(shoppingList -> {
             Long shoppingListId = shoppingList.getId();
             Long userId = shoppingList.getUserId();
@@ -190,26 +192,13 @@ public class ShoppingListServiceImpl implements ShoppingListService {
         });
     }
 
-    private ShoppingListResponseDTO convertToResponseDTO(ShoppingList shoppingList) {
-        return new ShoppingListResponseDTO(
-                shoppingList.getId(),
-                shoppingList.getName(),
-                shoppingList.getDescription(),
-                shoppingList.getCreatedDate(),
-                shoppingList.getStatus(),
-                shoppingList.getCollectionStatus(),
-                shoppingList.getReadinessStatus(),
-                null // Список элементов списка покупок
-        );
-    }
-
     private List<Product> findOptimalCombination(List<Product> availableProducts, int requiredQuantity) {
         List<Product> selectedProducts = new ArrayList<>();
         int remainingQuantity = requiredQuantity;
 
         availableProducts.sort((p1, p2) -> Integer.compare(p2.getVolumeOrWeight(), p1.getVolumeOrWeight())); // Сортируем от большего к меньшему
 
-        for (Product product : availableProducts) {
+        for (var product : availableProducts) {
             if (remainingQuantity <= 0) {
                 break;
             }
@@ -222,7 +211,7 @@ public class ShoppingListServiceImpl implements ShoppingListService {
 
         // Проверяем, есть ли остаток и можем ли его как-то покрыть
         if (remainingQuantity > 0) {
-            for (Product product : availableProducts) {
+            for (var product : availableProducts) {
                 int productVolumeOrWeight = product.getVolumeOrWeight();
                 if (productVolumeOrWeight >= remainingQuantity) {
                     selectedProducts.add(product);
